f7834b18a18e3c2a7b5d469744fc1de6d22f14c8
barkaleamol <amol_nj2_051@masai.school>
2022-02-12 15:05:31 +0530

    code updated

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,15 +7,33 @@ export default function Projects() {
   const projectData = [
     {
       id: "1",
+      name: "CryptoControl",
+      img: "crypto.png",
+      gh: "https://github.com/amolbarkale/cryptocontrol",
+      url: "https://amolbarkale.github.io/cryptocontrol/",
+      desc: "A one stop crypto currency application developed with react, redux and rapidAPI's where we can track most of the crypto news, prices and many more things.",
+      stack: "React, Redux, Ant Design, Chart.js",
+    },
+    {
+      id: "2",
+      name: "Shomissa",
+      img: "SMA.png",
+      gh: "https://github.com/amolbarkale/ShopmissA-project",
+      url: "https://streamline-acp24.vercel.app/",
+      desc: "A solo project developed in 5 days with user authentication for sign-up/sign-in page and password encryption in database.",
+      stack: "HTML, CSS, JavaScript, Express, MongoDB",
+    },
+    {
+      id: "3",
       name: "FAASOS - clone",
       img: "faasos.png",
       gh: "https://github.com/amolbarkale/Faasosdotcom",
       url: "https://faasosclone.netlify.app/",
       desc: "A fast food hotelwebsite, used for booking online food. In this collaborative project we tried to develop smooth user experience same as original website, along with 2 team members in 10 days.",
-      stack: "HTML, CSS, Javascript",
+      stack: "HTML, CSS, JavaScript",
     },
     {
-      id: "2",
+      id: "4",
       name: "Flipkart - clone",
       img: "FK.png",
       gh: "https://github.com/gsunil1996/flipkart_updated",
@@ -23,24 +41,7 @@ export default function Projects() {
       desc: "Flipkart clone s a collaborative project of four people where we developed functionalities in 6 days.",
       stack: "HTML, CSS, Javascript",
     },
-    {
-      id: "3",
-      name: "Shomissa",
-      img: "SMA.png",
-      gh: "https://github.com/amolbarkale/ShopmissA-project",
-      url: "https://streamline-acp24.vercel.app/",
-      desc: "A solo project developed in 5 days with user authentication for sign-up/sign-in page and password encryption in database.",
-      stack: "HTML, CSS, Javascript, NodeJS, Express, MongoDB",
-    },
-    {
-      id: "4",
-      name: "CryptoControl",
-      img: "crypto.png",
-      gh: "https://github.com/amolbarkale/cryptocontrol",
-      url: "https://amolbarkale.github.io/cryptocontrol/",
-      desc: "A one stop crypto currency application developed with react, redux and rapidAPI's where we can track most of the crypto news, prices and many more things.",
-      stack: "React, Redux, antd, chartjs",
-    },
+
     //     { id: "5", name: "Internshala - clone", img: "internshala2.png", gh:"https://github.com/Deependra-Negi/internshala_project", url:"https://pulkit0111.github.io/internshala_project/", desc: "An online internship and training platform. A collaborative project, developed along with 3 team members in 10 days.", stack:"HTML, CSS, JavaScript"}
   ];
 
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,83 +1,78 @@
-import React from 'react'
-import styled from 'styled-components'
-import Skill from './Skill'
-
+import React from "react";
+import styled from "styled-components";
+import Skill from "./Skill";
 
 export default function Skills() {
-    
-    const skillsData = [
-        { name: "HTML", url: "html.svg" },
-        { name: "CSS", url: "css.svg" },
-        { name: "JavaScript", url: "js.svg" },
-        { name: "React", url: "react.svg" },
-        { name: "Redux", url: "redux.svg" },
-        { name: "Node", url: "node.svg" },
-        { name: "Express", url: "express.svg" },
-        { name: "MongoDB", url: "mongo.svg" },
-        { name: "Git", url: "git.svg" },
-    ];
-    return (
-        <Cont id="skills">
-            <Heading>Skills</Heading>
-            <SkillsCont>
-                <Skill data={skillsData}/>
-            </SkillsCont>
-        </Cont>
-    )
+  const skillsData = [
+    { name: "HTML", url: "html.svg" },
+    { name: "CSS", url: "css.svg" },
+    { name: "JavaScript", url: "js.svg" },
+    { name: "React", url: "react.svg" },
+    { name: "Redux", url: "redux.svg" },
+    { name: "Express", url: "express.svg" },
+    { name: "MongoDB", url: "mongo.svg" },
+    { name: "Git", url: "git.svg" },
+  ];
+  return (
+    <Cont id="skills">
+      <Heading>Skills</Heading>
+      <SkillsCont>
+        <Skill data={skillsData} />
+      </SkillsCont>
+    </Cont>
+  );
 }
 
-
-
 const Cont = styled.div`
-background-color: #67687c;
-padding-bottom: 9rem;
-padding-top:20px;
-   clip-path: polygon(
-          0 8%,
-          /* left top */ 100% 0,
-          /* right top */ 100% 92%,
-          /* right bottom */ 0 100% /* left bottom */
-        );
-          @media (max-width: 1264px) {
-      clip-path: polygon(
-          0 7%,
-          /* left top */ 100% 0,
-          /* right top */ 100% 93%,
-          /* right bottom */ 0 100% /* left bottom */
-        );
-    }
-    @media (max-width: 768px) {
-       clip-path: polygon(
-          0 5%,
-          /* left top */ 100% 0,
-          /* right top */ 100% 95%,
-          /* right bottom */ 0 100% /* left bottom */
-        );
-    }
-    @media (max-width: 500px) {
-       clip-path: polygon(
-          0 2%,
-          /* left top */ 100% 0,
-          /* right top */ 100% 98%,
-          /* right bottom */ 0 100% /* left bottom */
-        );
-    }
-`
+  background-color: #67687c;
+  padding-bottom: 9rem;
+  padding-top: 20px;
+  clip-path: polygon(
+    0 8%,
+    /* left top */ 100% 0,
+    /* right top */ 100% 92%,
+    /* right bottom */ 0 100% /* left bottom */
+  );
+  @media (max-width: 1264px) {
+    clip-path: polygon(
+      0 7%,
+      /* left top */ 100% 0,
+      /* right top */ 100% 93%,
+      /* right bottom */ 0 100% /* left bottom */
+    );
+  }
+  @media (max-width: 768px) {
+    clip-path: polygon(
+      0 5%,
+      /* left top */ 100% 0,
+      /* right top */ 100% 95%,
+      /* right bottom */ 0 100% /* left bottom */
+    );
+  }
+  @media (max-width: 500px) {
+    clip-path: polygon(
+      0 2%,
+      /* left top */ 100% 0,
+      /* right top */ 100% 98%,
+      /* right bottom */ 0 100% /* left bottom */
+    );
+  }
+`;
 const Heading = styled.div`
-font-family: 'Poppins', sans-serif;
-    font-weight: 400;
-color: white;
-    padding-top: 5rem;
-    text-align:center ;
-    font-size: 2rem;
-`
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  color: white;
+  padding-top: 5rem;
+  text-align: center;
+  font-size: 2rem;
+`;
 const SkillsCont = styled.div`
-    margin: 4rem auto;
-    width: 50vw;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-    @media (max-width: 500px) {
-        margin: 2rem auto;
-    }
-`
+  margin: 4rem auto;
+  width: 50vw;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  @media (max-width: 500px) {
+    margin: 2rem auto;
+  }
+`;
